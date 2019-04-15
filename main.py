acc01ff0ee87c7396e682fed558dc2fb273989a9
qingzhi <sun2387353@163.com>
2019-04-15 14:46:31 +0800

    fix the problem of runtime_exception by close the converter manually

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ if __name__ == "__main__":
     # If you are using python2, the first two lines are needed.
     reload(sys)
     sys.setdefaultencoding('utf-8')
-
-    file_name = 'test'
+    file_name = 'news'
     input_path = 'input/'
     output_path = 'output/'
     corenlp_path = 'stanford-corenlp-full-2018-10-05'
@@ -20,7 +19,7 @@ if __name__ == "__main__":
     converter = FromCorenlpConverter(input_path + file_name + '.txt', '', corenlp_path, port)
     if not os.path.exists(output_path + file_name + '.xml'):
         converter.run()
-
+    converter.__del__()
     args = Namespace(command_name='batch', config=None, inputs=output_path + file_name + '.xml', nullactors=False, nullverbs=False, outputs=output_path + file_name + '_result.txt')
     # args = Namespace(command_name='batch', config=None, inputs='petrarch2/test-ch2.xml', nullactors=False, nullverbs=False, outputs=output_path + 'test-ch2' + '_result.txt')
     petrarch2_main(args)
