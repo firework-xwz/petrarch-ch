5859ab6f355317173569cdd37dd2eb5fbf012351
theForerunner <liet2026@163.com>
2019-05-21 12:37:11 +0800

    Merge branch 'master' of D:\OneDrive\Projects\Python\petrarch-ch with conflicts.

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ if __name__ == "__main__":
     output_path = 'output/'
     corenlp_path = 'stanford-corenlp-full-2018-10-05'
     port = 600
-
+    """
     converter = FromCorenlpConverter(input_path + file_name + '.txt', '', corenlp_path, port)
     
     converter.run()
 
     converter.__del__()
-
+    """
     args = Namespace(command_name='batch', config=None, inputs=output_path + file_name + '.xml', nullactors=False, nullverbs=False, outputs=output_path + file_name + '_result.txt')
     # args = Namespace(command_name='batch', config=None, inputs='petrarch2/test-ch2.xml', nullactors=False, nullverbs=False, outputs=output_path + 'test-ch2' + '_result.txt')
     petrarch2_main(args)
